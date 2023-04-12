7acc36fa83d2a29fd330ea7aeef45f1f0ba119bf
Daniel Salas <dvvsalas@gmail.com>
2023-04-12 15:11:56 -0700

    Making the two folders identical

--- app/models/spree_avatax_official/spree/order_decorator.rb
+++ app/models/spree_avatax_official/spree/order_decorator.rb
@@ -61,7 +61,7 @@ module SpreeAvataxOfficial
         return if response.success?
 
         error_message = response.value.body['messages'].map { |message| message['summary'] }.join('. ')
-
+      
         errors.add(:base, error_message)
 
         false
